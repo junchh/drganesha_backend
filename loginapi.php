65bc26fdb04a43b3377e889384d0bd2b8065f4af
Jun <cjunho9868@gmail.com>
2019-08-08 16:10:56 +0700

    login api done

--- loginapi.php
+++ loginapi.php
@@ -6,7 +6,16 @@ if(isset($_GET['username']) && isset($_GET['password'])){
     $username = $_GET['username'];
     $password = $_GET['password'];
 
-    
+    $stmt = $con->prepare('SELECT * FROM `users` WHERE `username` = ? AND `password` = ?');
+    $stmt->execute([$username, $password]);
+    if($stmt->rowCount()==1){
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        echo json_encode($data);
+    } else {
+        echo 'nodata';
+    }
+
 } else {
     echo 'errset';
 }
