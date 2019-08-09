bdffb93e3bf60bc0b9d0a742b6305990f88f80e5
Jun <cjunho9868@gmail.com>
2019-08-10 03:37:00 +0700

    tambah rating

--- entersessionapi.php
+++ entersessionapi.php
@@ -3,18 +3,28 @@
 
 require_once 'config.php';
 
-if(isset($_GET['pin']) && isset($_GET['session_id'])){
+if(isset($_GET['pin']) && isset($_GET['session_id']) && isset($_GET['rating'])){
     $pin = $_GET['pin'];
     $sessid = $_GET['session_id'];
+    $rating = $_GET['rating'];
 
     $stmt = $con->prepare('SELECT * FROM `private_sessions` WHERE `id` = ?');
     $stmt->execute([$sessid]);
     if($stmt->rowCount()==1){
         $data = $stmt->fetch(PDO::FETCH_ASSOC);
         if($data['pin']==$pin){
-            
+
             $stmt2 = $con->prepare('UPDATE `private_sessions` SET `status`=1 WHERE `id`=?');
             $stmt2->execute([$sessid]);
+
+            $stmt3 = $con->prepare('SELECT `rating` FROM `users` WHERE `id`=?');
+            $stmt3->execute([$data['tutor']]);
+
+            $dt = $stmt3->fetch(PDO::FETCH_ASSOC);
+            $m = ($dt['rating']+$rating)/2.0;
+
+            $stmt4 = $con->prepare('UPDATE `users` SET `rating`=? WHERE `id`=?');
+            $stmt4->execute([$m, $data['tutor']]);
             echo 'success';
         } else {
             echo 'wrongpin';
--- teacherenterapi.php
+++ teacherenterapi.php
@@ -1,2 +1,12 @@
-<?php
+<?php 
 
+require_once 'config.php';
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $stmt = $con->prepare('UPDATE `private_sessions` SET `status`=2 WHERE `id`=?');
+    $stmt->execute([$id]);
+    echo 'success';
+} else {
+    echo 'errset';
+}
